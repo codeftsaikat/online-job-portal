ad4e297da7c1c3bf27a8020aec23f4f47f827d16
unknown <soikatdey18@gmail.com>
2024-09-25 06:30:40 -0700

    added home design with tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 // import './App.css'
 import Header from './components/header/Header'
+import JobCard from './components/jobcard/JobCard'
 import Navbar from './components/navbar/Navbar'
 import SearchBar from './components/searchbar/SearchBar'
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Header/>
       <SearchBar/>
+      <JobCard/>
     </div>
   )
 }
