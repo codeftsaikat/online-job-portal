f74fafd2efbf7e0902c1de4dee1ef275eacca511
unknown <soikatdey18@gmail.com>
2024-09-25 07:55:47 -0700

    implement the job card section

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import Header from './components/header/Header'
 import JobCard from './components/jobcard/JobCard'
 import Navbar from './components/navbar/Navbar'
 import SearchBar from './components/searchbar/SearchBar'
-
+import jobData from '../public/jobDummyData'
 function App() {
   
+console.log(jobData);
 
   return (
     <div>
       <Navbar/>
       <Header/>
       <SearchBar/>
-      <JobCard/>
+      {
+        jobData.map((job)=><JobCard key={job.id} {...job}/>)
+      }
     </div>
   )
 }
--- src/components/jobcard/JobCard.jsx
+++ src/components/jobcard/JobCard.jsx
@@ -1,26 +1,45 @@
 import dayjs from "dayjs";
 
-function JobCard() {
-  const skills = ["Javascript", "React", "Nodejs"];
+function JobCard(props) {
+//   const {
+//     title,
+//     company,
+//     type,
+//     experience, 
+//     location,
+//     job_link,
+//     postedOn,
+//     skills,
+//   } = job;
+//   console.log(job.company);
+
+  const skillss = ["Javascript", "React", "Nodejs"];
   const date1 = dayjs(Date.now());
-  const diffInDays = date1.diff("2023-10-28", "day");
+  const diffInDays = date1.diff(props.postedOn, "day");
   return (
     <div className="mx-40 mb-4 flex justify-between px-6 py-4 bg-zinc-200 rounded-md hover:translate-y-1 hover:scale-103">
       <div className="space-y-2">
-        <h1 className="text-lg font-semibold">Frontend Developer - Amazon</h1>
-        <p  className="font-medium">Full Time &#x2022; Fresher &#x2022; In-office</p>
+        <h1 className="text-lg font-semibold">{props.title} - {props.company}</h1>
+        <p className="font-medium">
+          {props.type} &#x2022; {props.experience} &#x2022; {props.location}
+        </p>
         <div className="flex gap-2">
-          {skills.map((skill, idx) => (
+          {props.skills.map((skill, idx) => (
             <p
-            className="border border-gray-500 px-2 bg-transparent rounded-sm text-gray-600 font-medium"
-             key={idx}>{skill}</p>
+              className="border border-gray-500 px-2 bg-transparent rounded-sm text-gray-600 font-medium"
+              key={idx}
+            >
+              {skill}
+            </p>
           ))}
         </div>
       </div>
       <div className="flex items-center gap-4">
         <p className="font-medium">Posted {diffInDays} days ago</p>
-        <a href="">
-        <button className="border border-blue-600 bg-transparent text-blue-700 w-24 h-10 rounded-md font-medium hover:bg-blue-600 hover:text-gray-100">Apply</button>
+        <a href={props.job_link}>
+          <button className="border border-blue-600 bg-transparent text-blue-700 w-24 h-10 rounded-md font-medium hover:bg-blue-600 hover:text-gray-100">
+            Apply
+          </button>
         </a>
       </div>
     </div>
